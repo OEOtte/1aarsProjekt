1e1f44fcd346bd13e103f9a3c8e3343689b10201
alpa59 <117630853+alpa59@users.noreply.github.com>
2023-05-09 10:55:27 +0200

    changes

--- PolarSeafoodWarehouseManagment/src/model/Shipment.java
+++ PolarSeafoodWarehouseManagment/src/model/Shipment.java
@@ -6,10 +6,20 @@ public class Shipment {
 
 	private List<Staff> staffOnShipment;
 	private Freight freight;
+	private List<ShipmentLine> shipmentLines;
 	
 	
 	public Shipment(List<Staff> staffs, Freight freight) {
 		this.freight = freight;
 		this.staffOnShipment = staffs;
+	}
+	
+	public boolean addProductToAOrderline(Product product, int quantity) {
+		boolean res = false;
+		for(int i = 0; i < shipmentLines.size(); i++) {
+			if(shipmentLines.get(i).getProduct().equals(product)){
+				shipmentLines.get(i).increaseQty(quantity);
+			}
+		}
 	}
 }
