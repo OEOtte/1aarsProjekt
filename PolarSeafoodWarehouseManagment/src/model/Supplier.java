5f6c818a2e2f1e200c93b7642d59e9883f951c1a
UCNoscar <117630926+UCNoscar@users.noreply.github.com>
2023-05-09 10:54:40 +0200

    supplier

---
+++ PolarSeafoodWarehouseManagment/src/model/Supplier.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Supplier {
+	private String name;
+	private String phoneNo;
+	private String email;
+	private String country;
+	
+	public Supplier(String name, String phoneNo, String email, String country) {
+		super();
+		this.name = name;
+		this.phoneNo = phoneNo;
+		this.email = email;
+		this.country = country;
+	}
+	
+}
