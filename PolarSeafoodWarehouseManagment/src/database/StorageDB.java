a7daeb5ab98a685b52d2fd87bd9d4eadb40e4c80
Thomas-Frederiksen <thomas.frederiksen07@gmail.com>
2023-05-09 14:26:40 +0200

    Ligma

--- PolarSeafoodWarehouseManagment/src/database/StorageDB.java
+++ PolarSeafoodWarehouseManagment/src/database/StorageDB.java
@@ -6,6 +6,6 @@ public class StorageDB implements StorageDBIF{
 	private static final String FIND_ALL_Q = "select * from Lot";
 	private PreparedStatement findAllPS;
 
-	private static String FIND_BY_PRIORITY_Q = "select * from Lot where priority = ?;";
+	private static String FIND_BY_PRIORITY_Q = "select * from Lot where priority = ?";
 	private PreparedStatement findByPriorityPS;
 }
--- PolarSeafoodWarehouseManagment/src/model/Lot.java
+++ PolarSeafoodWarehouseManagment/src/model/Lot.java
@@ -3,6 +3,20 @@ package model;
 public class Lot {
 
 	private String lotNumber;
+	private Warehouse warehouse;
+	
+	public Lot(String lotNumber, Warehouse warehouse) {
+		this.lotNumber = lotNumber;
+		this.warehouse = warehouse;
+	}
+
+	public Warehouse getWarehouse() {
+		return warehouse;
+	}
+
+	public void setWarehouse(Warehouse warehouse) {
+		this.warehouse = warehouse;
+	}
 
 	public String getLotNumber() {
 		return lotNumber;
