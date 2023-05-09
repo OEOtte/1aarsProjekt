2d253152d701da821ad370ba8a7c545591d1e751
UCNoscar <117630926+UCNoscar@users.noreply.github.com>
2023-05-09 12:27:52 +0200

    getters
    getters

--- PolarSeafoodWarehouseManagment/src/model/Product.java
+++ PolarSeafoodWarehouseManagment/src/model/Product.java
@@ -22,4 +22,70 @@ public class Product {
 		this.weight = weight;
 		this.minStock = minStock;
 	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getItemNumber() {
+		return itemNumber;
+	}
+
+	public void setItemNumber(String itemNumber) {
+		this.itemNumber = itemNumber;
+	}
+
+	public String getBarcode() {
+		return barcode;
+	}
+
+	public void setBarcode(String barcode) {
+		this.barcode = barcode;
+	}
+
+	public String getCountryOfOrigin() {
+		return countryOfOrigin;
+	}
+
+	public void setCountryOfOrigin(String countryOfOrigin) {
+		this.countryOfOrigin = countryOfOrigin;
+	}
+
+	public Double getPercentOfGlaze() {
+		return percentOfGlaze;
+	}
+
+	public void setPercentOfGlaze(Double percentOfGlaze) {
+		this.percentOfGlaze = percentOfGlaze;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public int getMinStock() {
+		return minStock;
+	}
+
+	public void setMinStock(int minStock) {
+		this.minStock = minStock;
+	}
+	
+	
 }
