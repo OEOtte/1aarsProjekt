c45be6366e7a9c7282126c5bf46b1dd2614ce281
alpa59 <117630853+alpa59@users.noreply.github.com>
2023-05-23 23:04:36 +0200

    Updates

--- PolarSeafoodWarehouseManagment/src/gui/MainGui.java
+++ PolarSeafoodWarehouseManagment/src/gui/MainGui.java
@@ -34,13 +34,15 @@ import javax.swing.DefaultListModel;
 
 import java.awt.FlowLayout;
 import model.*;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.LineBorder;
 
 public class MainGui extends JFrame {
 
 	private static MainGui frame;
 	private JPanel contentPane;
 	private JTextField txtProductName;
-	private JTextField txtBarcode;
+	private JTextField txtWarehouse;
 	private JTable tblLotLines;
 	private JTextField textQuantity;
 	private StorageCtrl storageCtrl;
@@ -105,7 +107,7 @@ public class MainGui extends JFrame {
 		setTitle("Polar Seafood Ukraine");
 		setFont(new Font("Dialog", Font.PLAIN, 10));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 778, 648);
+		setBounds(100, 100, 880, 746);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -121,6 +123,7 @@ public class MainGui extends JFrame {
 		panel.add(lblNewLabel);
 
 		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(null);
 		contentPane.add(panel_1, BorderLayout.SOUTH);
 		GridBagLayout gbl_panel_1 = new GridBagLayout();
 		gbl_panel_1.columnWidths = new int[] { 452, 0 };
@@ -140,11 +143,12 @@ public class MainGui extends JFrame {
 		scrollPane.setViewportView(tblLotLines);
 
 		JPanel panel_2 = new JPanel();
+		panel_2.setBorder(null);
 		contentPane.add(panel_2, BorderLayout.CENTER);
 		GridBagLayout gbl_panel_2 = new GridBagLayout();
-		gbl_panel_2.columnWidths = new int[] { 35, 256, 191, 293, 0 };
+		gbl_panel_2.columnWidths = new int[] { 0, 35, 256, 191, 293, 0 };
 		gbl_panel_2.rowHeights = new int[] { 49, 0, 0, 0, 0, 43, 0 };
-		gbl_panel_2.columnWeights = new double[] { 0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE };
+		gbl_panel_2.columnWeights = new double[] { 0.0, 0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE };
 		gbl_panel_2.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
 		panel_2.setLayout(gbl_panel_2);
 
@@ -153,16 +157,23 @@ public class MainGui extends JFrame {
 		GridBagConstraints gbc_findProduct = new GridBagConstraints();
 		gbc_findProduct.anchor = GridBagConstraints.WEST;
 		gbc_findProduct.insets = new Insets(0, 0, 5, 5);
-		gbc_findProduct.gridx = 1;
+		gbc_findProduct.gridx = 2;
 		gbc_findProduct.gridy = 0;
 		panel_2.add(findProduct, gbc_findProduct);
+		
+		JLabel lblProductName = new JLabel("Product Name:");
+		GridBagConstraints gbc_lblProductName = new GridBagConstraints();
+		gbc_lblProductName.insets = new Insets(0, 0, 5, 5);
+		gbc_lblProductName.anchor = GridBagConstraints.EAST;
+		gbc_lblProductName.gridx = 1;
+		gbc_lblProductName.gridy = 1;
+		panel_2.add(lblProductName, gbc_lblProductName);
 
 		txtProductName = new JTextField();
-		txtProductName.setText("Product Name");
 		GridBagConstraints gbc_txtProductName = new GridBagConstraints();
 		gbc_txtProductName.insets = new Insets(0, 0, 5, 5);
 		gbc_txtProductName.fill = GridBagConstraints.HORIZONTAL;
-		gbc_txtProductName.gridx = 1;
+		gbc_txtProductName.gridx = 2;
 		gbc_txtProductName.gridy = 1;
 		panel_2.add(txtProductName, gbc_txtProductName);
 		txtProductName.setColumns(10);
@@ -170,7 +181,7 @@ public class MainGui extends JFrame {
 		JButton btnRegisterShipment = new JButton("REGISTER SHIPMENT");
 		GridBagConstraints gbc_btnRegisterShipment = new GridBagConstraints();
 		gbc_btnRegisterShipment.insets = new Insets(0, 0, 5, 0);
-		gbc_btnRegisterShipment.gridx = 3;
+		gbc_btnRegisterShipment.gridx = 4;
 		gbc_btnRegisterShipment.gridy = 1;
 		panel_2.add(btnRegisterShipment, gbc_btnRegisterShipment);
 		btnRegisterShipment.addActionListener(new ActionListener() {
@@ -179,21 +190,28 @@ public class MainGui extends JFrame {
 			}
 		});
 		btnRegisterShipment.setFont(new Font("Tahoma", Font.BOLD, 14));
+		
+		JLabel lblWarehouse = new JLabel("Warehouse:");
+		GridBagConstraints gbc_lblWarehouse = new GridBagConstraints();
+		gbc_lblWarehouse.insets = new Insets(0, 0, 5, 5);
+		gbc_lblWarehouse.anchor = GridBagConstraints.EAST;
+		gbc_lblWarehouse.gridx = 1;
+		gbc_lblWarehouse.gridy = 2;
+		panel_2.add(lblWarehouse, gbc_lblWarehouse);
 
-		txtBarcode = new JTextField();
-		txtBarcode.setText("Barcode");
-		GridBagConstraints gbc_txtBarcode = new GridBagConstraints();
-		gbc_txtBarcode.insets = new Insets(0, 0, 5, 5);
-		gbc_txtBarcode.fill = GridBagConstraints.HORIZONTAL;
-		gbc_txtBarcode.gridx = 1;
-		gbc_txtBarcode.gridy = 2;
-		panel_2.add(txtBarcode, gbc_txtBarcode);
-		txtBarcode.setColumns(10);
+		txtWarehouse = new JTextField();
+		GridBagConstraints gbc_txtWarehouse = new GridBagConstraints();
+		gbc_txtWarehouse.insets = new Insets(0, 0, 5, 5);
+		gbc_txtWarehouse.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtWarehouse.gridx = 2;
+		gbc_txtWarehouse.gridy = 2;
+		panel_2.add(txtWarehouse, gbc_txtWarehouse);
+		txtWarehouse.setColumns(10);
 
 		JButton btnReserveProduct = new JButton("RESERVE PRODUCT");
 		GridBagConstraints gbc_btnReserveProduct = new GridBagConstraints();
 		gbc_btnReserveProduct.insets = new Insets(0, 0, 5, 0);
-		gbc_btnReserveProduct.gridx = 3;
+		gbc_btnReserveProduct.gridx = 4;
 		gbc_btnReserveProduct.gridy = 2;
 		panel_2.add(btnReserveProduct, gbc_btnReserveProduct);
 		btnReserveProduct.addActionListener(new ActionListener() {
@@ -202,13 +220,20 @@ public class MainGui extends JFrame {
 			}
 		});
 		btnReserveProduct.setFont(new Font("Tahoma", Font.BOLD, 14));
+		
+		JLabel lblQuantity = new JLabel("Quantity");
+		GridBagConstraints gbc_lblQuantity = new GridBagConstraints();
+		gbc_lblQuantity.insets = new Insets(0, 0, 5, 5);
+		gbc_lblQuantity.anchor = GridBagConstraints.EAST;
+		gbc_lblQuantity.gridx = 1;
+		gbc_lblQuantity.gridy = 3;
+		panel_2.add(lblQuantity, gbc_lblQuantity);
 
 		textQuantity = new JTextField();
-		textQuantity.setText("Quantity");
 		GridBagConstraints gbc_textQuantity = new GridBagConstraints();
 		gbc_textQuantity.insets = new Insets(0, 0, 5, 5);
 		gbc_textQuantity.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textQuantity.gridx = 1;
+		gbc_textQuantity.gridx = 2;
 		gbc_textQuantity.gridy = 3;
 		panel_2.add(textQuantity, gbc_textQuantity);
 		textQuantity.setColumns(10);
@@ -222,16 +247,22 @@ public class MainGui extends JFrame {
 		btnPickProduct.setFont(new Font("Tahoma", Font.BOLD, 14));
 		GridBagConstraints gbc_btnPickProduct = new GridBagConstraints();
 		gbc_btnPickProduct.insets = new Insets(0, 0, 5, 0);
-		gbc_btnPickProduct.gridx = 3;
+		gbc_btnPickProduct.gridx = 4;
 		gbc_btnPickProduct.gridy = 3;
 		panel_2.add(btnPickProduct, gbc_btnPickProduct);
+		
+		JLabel lblNewLabel_1 = new JLabel("-");
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.gridx = 2;
+		gbc_lblNewLabel_1.gridy = 4;
+		panel_2.add(lblNewLabel_1, gbc_lblNewLabel_1);
 
 		init();
 	}
 
 	private void init() {
 		storageCtrl = new StorageCtrl();
-		// lltm = new ProductListTableModel(null);
 		updateProductList();
 
 	}
